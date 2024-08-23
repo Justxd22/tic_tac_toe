fc77b457e826b9a7abde1e3e10a319cf02e0a944
madiocre <wolvirex@gmail.com>
2024-08-24 01:21:54 +0300

    Client Side: Fixed Login and Register Style

--- Client/src/components/Login.tsx
+++ Client/src/components/Login.tsx
@@ -1,7 +1,7 @@
-import '../assets/stylesheets/Login.css';
-import { FC, useState } from 'react';
+import React, { FC, useState } from "react";
 import { FaUser, FaLock, FaArrowLeft } from "react-icons/fa";  
 import Logo from '../assets/Images/Text_Logo.png';  
+import '../assets/stylesheets/Login.css';
 
 const Login: FC = () => {
   const [username, setUsername] = useState('');
@@ -10,7 +10,6 @@ const Login: FC = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    // Send the login request to the backend
     try {
       const response = await fetch('http://127.0.0.1:3000/login', {
         method: 'POST',
@@ -23,11 +22,9 @@ const Login: FC = () => {
       const data = await response.json();
 
       if (response.ok) {
-        // Handle successful login (e.g., redirect or store tokens)
         console.log('Login successful:', data);
         window.location.href = '/tictactoe';
       } else {
-        // Handle login error
         console.error('Login failed:', data.message);
         alert(`Login failed: ${data.message}`);
       }
@@ -39,41 +36,31 @@ const Login: FC = () => {
   return (
     <>
       <img src={Logo} alt="Logo" className="logo" />
+
       <div className="wrapper">
         <form onSubmit={handleSubmit}>
           <h1>Login</h1>
           <div className="input-box">
-            <input
-              type="text"
-              placeholder="Username"
-              required
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <FaUser className="icon"/>
+            <input type="text" placeholder="Username" required />
+            <FaUser className="icon" />
           </div>
           <div className="input-box">
-            <input
-              type="password"
-              placeholder="Password"
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <FaLock className="icon"/>
+            <input type="password" placeholder="Password" required />
+            <FaLock className="icon" />
           </div>
           <button type="submit">Login</button>
           <div className="register-link">
-            <p>Don't have an account? <a href="/register">Register</a></p>
+            <p>Don't have an account? <a href="#">Register</a></p>
           </div>
         </form>
       </div>
+
       <a href="#" className="back-button">
         <FaArrowLeft className="icon" />
         Back
       </a>
     </>
   );
-}
+};
 
 export default Login;
