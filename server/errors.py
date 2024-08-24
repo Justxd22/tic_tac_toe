8b4e94acd18aa64129a3ad0c8fe51f2dface5c16
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-08-24 17:59:20 +0300

    Implement application configuration, error handling, and middleware
    - Add `default.py`, `production.py`, and `development.py` for application configuration in different environments
    - Implement `middleware.py` to apply custom middleware to the application
    - Create `errors.py` to handle common HTTP errors (404, 500, 401, 403) and return appropriate JSON responses
    - Add `template_renderer.py` to handle rendering of HTML templates with context
    - Include `.gitkeep` files in empty directories to ensure they are tracked by Git
    - Define `events.py` to handle SocketIO events in the application
    - Update `requirements.txt` with necessary Python packages for the application

--- server/errors.py
+++ server/errors.py
@@ -1,27 +1,59 @@
-"""All error codes here."""
-from flask import Blueprint, jsonify
+"""Module for handling error codes in the application."""
+from flask import Blueprint, jsonify, Response
 
 error = Blueprint('errors', __name__)
 
 @error.errorhandler(404)
-def not_found(error):
-    """Not found handler."""
+def not_found(error) -> Response:
+    """
+    Handle 404 errors.
+
+    Args:
+        error: The error object.
+
+    Returns:
+        A JSON response with the error message and the HTTP status code.
+    """
     return jsonify({"error": "Not found"}), 404
 
 
 @error.errorhandler(500)
-def internal_server_error(error):
-    """Handler for 500 Internal Server Error."""
+def internal_server_error(error) -> Response:
+    """
+    Handle 500 errors.
+
+    Args:
+        error: The error object.
+
+    Returns:
+        A JSON response with the error message and the HTTP status code.
+    """
     return jsonify({"error": "Internal Server Error"}), 500
 
 
 @error.errorhandler(401)
-def unauthorized(error) -> str:
-    """Unauthorized handler."""
-    return jsonify({"error": "UnauthorizedDD"}), 401
+def unauthorized(error) -> Response:
+    """
+    Handle 401 errors.
+
+    Args:
+        error: The error object.
+
+    Returns:
+        A JSON response with the error message and the HTTP status code.
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 @error.errorhandler(403)
-def forbidden(error) -> str:
-    """Forbidden handler."""
+def forbidden(error) -> Response:
+    """
+    Handle 403 errors.
+
+    Args:
+        error: The error object.
+
+    Returns:
+        A JSON response with the error message and the HTTP status code.
+    """
     return jsonify({"error": "Forbidden"}), 403
