8da131d0d1f30970c3f324f0e306453151ef324f
justxd22 <xdjust18@gmail.com>
2024-08-30 18:26:52 +0300

    Add status endpoint + fix sockets url

--- server/web_dynamic/template_renderer.py
+++ server/web_dynamic/template_renderer.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, send_from_directory, redirect, url_for, session
+from flask import Blueprint, send_from_directory, redirect, url_for, session, jsonify
 import os
 
 web_bp = Blueprint('web_dynamic', __name__, static_folder='../static')
@@ -27,6 +27,11 @@ def tictactoe():
     # if 'username' in session:
     #     return redirect(url_for('web_dynamic.serve', path='/tictactoe'))
     return send_from_directory(web_bp.static_folder, 'index.html')
+
+@web_bp.route('/status')
+def status():
+    data = { "status": "ok", "msg": "Hello Human!"}
+    return jsonify(data)
 
 # @web_bp.route('/', defaults={'path': ''})
 @web_bp.route('/')
