76dee53868b04b8d038060adc4c72fdf075f24ec
IbnuJabir <kedirjabir12@gmail.com>
2024-08-30 13:38:20 +0300

    update config

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        newrocker: ['"New Rocker"', 'cursive'],
+      },
+      backgroundImage: {
+        "result-background": "url('./src/assets/images/resultBackground.png')",
+      },
+      keyframes: {
+        pulse: {
+          "0%, 100%": { boxShadow: "0 0 0 0 var(--pulse-color)" },
+          "50%": { boxShadow: "0 0 0 8px var(--pulse-color)" },
+        },
+      },
+      animation: {
+        pulse: "pulse var(--duration) ease-out infinite",
+      },
+    },
   },
   plugins: [],
-}
-
+};
